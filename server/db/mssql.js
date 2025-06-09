389d59096ee189bd0d652cfeef76fdaf70689c81
Rahat <rahatalisoomro10@gmail.com>
2025-06-10 00:26:57 +0500

    DB Setup

--- server/db/mssql.js
+++ server/db/mssql.js
@@ -1,23 +1,20 @@
-// db/mssql.js
+
 const sql = require('mssql');
 require('dotenv').config();
+console.log('DB_SERVER:', process.env.DB_SERVER);
+console.log('DB_User:', process.env.DB_USER);
+console.log('DB_Pasword:', process.env.DB_PASSWORD);
 
 const config = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
   database: process.env.DB_DATABASE,
   options: {
     encrypt: false,
     trustServerCertificate: true,
     trustedConnection: true,
-  },
-  authentication:{
-    type: 'ntlm',
-    options: {
-      domain: '', // Leave blank unless you're on a domain
-      userName: '', // Leave blank to use current user
-      password: '', // Leave blank to use current user
-        }
-    }
+  }
 };
 
 const pool = new sql.ConnectionPool(config);
